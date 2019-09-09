8c697366b3ac555d8bbb14e1b88c05f7a35efa2f
Сидоренко Славик <termenator2001@gmail.com>
2019-09-09 18:25:00 +0300

    2

--- SnakeV1/Program.cs
+++ SnakeV1/Program.cs
@@ -8,11 +8,11 @@ using NConsoleGraphics;
 
 namespace SnakeV1
 {
-    internal class Canvas
+    class Canvas
     {
         private readonly uint _color;
-        public int ClientWidth { get; }
-        public int ClientHeigth { get; }
+        public int ClientWidth;
+        public int ClientHeigth;
 
         public Canvas(uint color, int clientWidth, int clientHeigth)
         {
@@ -41,23 +41,23 @@ namespace SnakeV1
         }
         public void Move()
         {
-            if (Input.IsKeyDown(Keys.RIGHT))
+            if (Input.IsKeyDown(Keys.KEY_D))
             {
                 _x += 30;
             }
-            if (Input.IsKeyDown(Keys.LEFT))
+            else if (Input.IsKeyDown(Keys.KEY_A))
             {
                 _x -= 30;
             }
-            if (Input.IsKeyDown(Keys.DOWN))
+            else if (Input.IsKeyDown(Keys.KEY_S))
             {
                 _y += 30;
             }
-            if (Input.IsKeyDown(Keys.UP))
+            else if (Input.IsKeyDown(Keys.KEY_W))
             {
                 _y -= 30;
             }
-            if (_x > _canvas.ClientWidth)
+            if (_x > _canvas.ClientWidth - 30)
             {
                 _x = 0;
             }
@@ -65,7 +65,7 @@ namespace SnakeV1
             {
                 _x = _canvas.ClientWidth - 30;
             }
-            if (_y > _canvas.ClientHeigth)
+            if (_y > _canvas.ClientHeigth - 400)
             {
                 _y = 0;
             }
@@ -79,6 +79,26 @@ namespace SnakeV1
             cg.FillRectangle(_color, _x, _y, size, size);
         }
     }
+    class Apple
+    {
+        private const int size = 30;
+        private readonly uint _color;
+        public int _x;
+        public int _y;
+        public readonly Canvas _canvas;
+        public Apple(uint color, Canvas canvas)
+        {
+            _color = color;
+            _canvas = canvas;
+        }
+        public void Spawn(ConsoleGraphics cg)
+        {
+            Random random = new Random();
+            _x = random.Next(10, _canvas.ClientWidth - 30);
+            _y= random.Next(10, _canvas.ClientHeigth - 400);
+            cg.FillRectangle(_color, _x, _y, size, size);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -86,15 +106,17 @@ namespace SnakeV1
             Console.CursorVisible = false;
             ConsoleGraphics cg = new ConsoleGraphics();
             Canvas canvas = new Canvas(0xFFbdc1c9, cg.ClientWidth, cg.ClientWidth);
-            Head head = new Head(0xFFFF0000, 0, 0, canvas);
+            Apple apple = new Apple(0xFF7f711, canvas);
+            Head head = new Head(0xFFFF0000, 60, 0, canvas);
             while (true)
             {
+                apple.Spawn(cg);
                 head.Move();
                 canvas.Render(cg);
                 head.Render(cg);
                 
                 cg.FlipPages();
-                Thread.Sleep(100);
+                Thread.Sleep(130);
             }
         }
     }
